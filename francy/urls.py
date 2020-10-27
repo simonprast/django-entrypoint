85e3020443015a890236613b295bc66e4da45013
Simon <simon.prast.sp@gmail.com>
2020-10-27 10:59:02 +0100

    Remove GraphQL URL
    Remove GraphQL URL at /graphql and graphene_django import.

--- francy/urls.py
+++ francy/urls.py
@@ -17,9 +17,6 @@ from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
-from graphene_django.views import GraphQLView
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
