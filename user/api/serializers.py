b266782b16e30bea7380935ef0bc40c63a192777
Simon <simon.prast.sp@gmail.com>
2020-10-30 15:58:05 +0100

    Add user serializers
    Add two user serializers:
    - UserSerializer, which includes all User fields expect password and is_admin
    - RegisterUserSerializer, which creates a new User object using the create_user function on saving.

---
+++ user/api/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from user.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = ['password', 'is_admin']
+
+
+class RegisterUserSerializer(serializers.ModelSerializer):
+    # password = serializers.CharField()
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+
+    def save(self):
+        User.create_user(
+            username=self.validated_data.get('username'),
+            email=self.validated_data.get('email'),
+            password=self.validated_data.get('password')
+        )
