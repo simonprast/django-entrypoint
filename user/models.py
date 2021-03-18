03713164a187432cce27e8e023ae0649828815cb
Simon <simon.prast.sp@gmail.com>
2021-03-18 13:18:02 +0100

    Add default superuser creation error handling
    Add error handling for the creation and updating process of the default superuser.
    
    The unique attribute check was missing on changing an existing superuser's data. For this, this check now exists as the method unique_fail(), returning True if any user which is not the default superuser has the attributes given at settings.ADMIN_USER and .ADMIN_MAIL.

--- user/models.py
+++ user/models.py
@@ -88,7 +88,8 @@ class User(AbstractBaseUser):
         # The email field is optional for administrative users though,
         # therefore the unique attribute cannot be used here.
         if self.email is not None:
-            if User.objects.filter(email=self.email).exclude(username=self.username).exists():
+            if User.objects.filter(email=self.email).exclude(id=self.id).exists():
+                print(User.objects.filter(email=self.email).exclude(username=self.username))
                 raise exceptions.ValidationError('User with this E-Mail already exits.')
 
         super(User, self).save(*args, **kwargs)
@@ -126,63 +127,49 @@ def create_admin_user():
         # If the superuser object should be persistent, the existing object is used and updated.
         # Else, the existing superuser object and its children are deleted.
         if settings.ADMIN_PERSISTENT:
-            update = False
-            if not user.username == settings.ADMIN_USER:
-                print(f'{Fore.GREEN}Changing existing default superuser\'s name:')
-                print(f'From {user.username} to {settings.ADMIN_USER}{Style.RESET_ALL}')
+            # Check wether another account with this username or email address already exists.
+            username_unique_fail, email_unique_fail = unique_fail()
 
-                user.username = settings.ADMIN_USER
-                update = True
+            if not username_unique_fail and not email_unique_fail:
+                update = False
+                if not user.username == settings.ADMIN_USER:
+                    print(f'{Fore.GREEN}Changing existing default superuser\'s name:')
+                    print(f'From {user.username} to {settings.ADMIN_USER}{Style.RESET_ALL}')
 
-            if not user.email == settings.ADMIN_MAIL:
-                print(f'{Fore.GREEN}Changing existing default superuser\'s email:')
-                print(f'From {user.email} to {settings.ADMIN_MAIL}{Style.RESET_ALL}')
+                    user.username = settings.ADMIN_USER
+                    update = True
 
-                user.email = settings.ADMIN_MAIL
-                update = True
+                if not user.email == settings.ADMIN_MAIL:
+                    print(f'{Fore.GREEN}Changing existing default superuser\'s email:')
+                    print(f'From {user.email} to {settings.ADMIN_MAIL}{Style.RESET_ALL}')
 
-            if not user.check_password(settings.ADMIN_PASSWORD):
-                print(f'{Fore.GREEN}Updating existing default superuser\'s password.{Style.RESET_ALL}')
+                    user.email = settings.ADMIN_MAIL
+                    update = True
 
-                user.set_password(settings.ADMIN_PASSWORD)
-                update = True
+                if not user.check_password(settings.ADMIN_PASSWORD):
+                    print(f'{Fore.GREEN}Updating existing default superuser\'s password.{Style.RESET_ALL}')
 
-            if update:
-                user.save()
+                    user.set_password(settings.ADMIN_PASSWORD)
+                    update = True
 
-            print(f'{Fore.GREEN}Default superuser account:')
-            print(f'Username: {settings.ADMIN_USER}')
-            print(f'E-Mail: {settings.ADMIN_MAIL}{Style.RESET_ALL}\n')
+                if update:
+                    user.save()
+                    print('')
+
+                print(f'{Fore.BLUE}Default superuser account:')
+                print(f'Username: {user.username}')
+                print(f'E-Mail: {user.email}{Style.RESET_ALL}\n')
+            else:
+                print(f'{Fore.BLUE}Keeping default superuser account attributes:')
+                print(f'Username: {user.username}')
+                print(f'E-Mail: {user.email}{Style.RESET_ALL}\n')
         else:
             User.objects.filter(default_superuser=True).delete()
     else:
         # Check wether another account with this username or email address already exists.
-        username_unique_fail = False
-        email_unique_fail = False
+        username_unique_fail, email_unique_fail = unique_fail()
 
-        if User.objects.filter(username=settings.ADMIN_USER).exists():
-            username_unique_fail = True
-
-        if User.objects.filter(email=settings.ADMIN_MAIL).exists():
-            email_unique_fail = True
-
-        # ... and show an error message accordingly.
-        if username_unique_fail or email_unique_fail:
-            # ... A formatted string literal or f-string is a string literal that is prefixed with 'f' or 'F'.
-            # These strings may contain replacement fields, which are expressions delimited by curly braces {}.
-            # While other string literals always have a constant value, formatted strings are really expressions
-            # evaluated at run time.
-            print(f'{Fore.RED}Could not create default superuser account:')
-
-            if username_unique_fail:
-                print('Username already exists on non-superuser account.', end='')
-
-            if email_unique_fail:
-                print('E-Mail already exists on non-superuser account.', end='')
-
-            print(f'{Style.RESET_ALL}\n')
-
-        else:
+        if not username_unique_fail and not email_unique_fail:
             User.objects.create_superuser(
                 username=settings.ADMIN_USER,
                 email=settings.ADMIN_MAIL,
@@ -193,3 +180,33 @@ def create_admin_user():
             print(f'{Fore.GREEN}Created default superuser account:')
             print(f'Username: {settings.ADMIN_USER}')
             print(f'E-Mail: {settings.ADMIN_MAIL}{Style.RESET_ALL}\n')
+
+
+def unique_fail():
+    # Check wether another account with this username or email address already exists.
+    username_unique_fail = False
+    email_unique_fail = False
+
+    if User.objects.filter(username=settings.ADMIN_USER, default_superuser=False).exists():
+        username_unique_fail = True
+
+    if User.objects.filter(email=settings.ADMIN_MAIL, default_superuser=False).exists():
+        email_unique_fail = True
+
+    # ... and show an error message accordingly.
+    if username_unique_fail or email_unique_fail:
+        # ... A formatted string literal or f-string is a string literal that is prefixed with 'f' or 'F'.
+        # These strings may contain replacement fields, which are expressions delimited by curly braces {}.
+        # While other string literals always have a constant value, formatted strings are really expressions
+        # evaluated at run time.
+        print(f'{Fore.RED}Could not update default superuser account:', end='')
+
+        if username_unique_fail:
+            print('\nUsername already exists on non-superuser account.', end='')
+
+        if email_unique_fail:
+            print('\nE-Mail already exists on non-superuser account.', end='')
+
+        print(f'{Style.RESET_ALL}\n')
+
+    return username_unique_fail, email_unique_fail
