0cd480d9270d5830b8046456d2be3528700f3b14
petute <daniel.petutschnigg@gmail.com>
2021-03-25 14:58:27 +0100

    Change MailModel

--- mailing/models.py
+++ mailing/models.py
@@ -4,11 +4,12 @@ from user.models import User
 
 # Create your models here.
 
-
 class MailModel(models.Model):
-    template = models.CharField(max_length=100)
     user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    to_email = models.EmailField(blank=False, null=True)
     from_email = models.EmailField(blank=True, null=True)
+    subject = models.TextField(null=True)
+    message = models.TextField(null=True)
 
     def __str__(self):
         return self.user.username
