e1ad76174a9e17d1ce8842a0769850b9539cac33
petute <daniel.petutschnigg@gmail.com>
2021-03-26 10:12:27 +0100

    Add requirement Added django-mail-templated requirement.

--- mailing/views.py
+++ mailing/views.py
@@ -7,7 +7,7 @@ from francy.settings import DEFAULT_FROM_EMAIL
 
 # Create your views here.
 
-
+# sendMail sends an email and saves it into the database.
 def sendMail(template, from_email, to_email):
     try:
         user = User.objects.get(email=to_email)
@@ -21,7 +21,6 @@ def sendMail(template, from_email, to_email):
 
     if from_email == None or from_email == '':
         from_email = DEFAULT_FROM_EMAIL
-
 
     message.load_template()
     message.render()
